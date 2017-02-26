6384bba6062acd878d8fe99b93428b344288bc68
Ziad Tamim <tamim.ziad@gmail.com>
2017-02-26 18:23:47 +0000

    Fix bug to restore completed transactions

--- PurchaseManager/Classes/PurchaseController.swift
+++ PurchaseManager/Classes/PurchaseController.swift
@@ -13,21 +13,29 @@ import BoltsSwift
 class PurchaseController: NSObject {
     
     var currentProductsRequestHandler: ITSProductsRequestHandler!
-    
     var validProducts: Set<SKProduct>?
+    var transactionObserver: PaymentTransactionObserver!
+    
+    lazy var paymentQueue: SKPaymentQueue = {
+        return SKPaymentQueue.default()
+    }()
+    
+    override init() {
+        super.init()
+        transactionObserver = PaymentTransactionObserver()
+        paymentQueue.add(transactionObserver)
+    }
     
     ///--------------------------------------
     // MARK - Products
     ///--------------------------------------
     
-    func findPriceProduct(_ productIdentifier: String) -> String?
-    {
+    func findPriceProduct(_ productIdentifier: String) -> String? {
         let products = validProducts?.filter { $0.productIdentifier == productIdentifier }
         return products?.first?.localizedPrice()
     }
     
-    func findProductsAsyncWithIdentifiers(_ productIdentifiers: Set<String>, completion: @escaping (Bool) -> ())
-    {
+    func findProductsAsyncWithIdentifiers(_ productIdentifiers: Set<String>, completion: @escaping (Bool) -> ()) {
         let request = SKProductsRequest(productIdentifiers: productIdentifiers)
         currentProductsRequestHandler = ITSProductsRequestHandler(request: request)
         currentProductsRequestHandler.findProductsAsync { [weak self] (result, error) in
@@ -39,49 +47,34 @@ class PurchaseController: NSObject {
         }
     }
     
-    func buyProductAsyncWithIdentifier(_ productIdentifier: String) -> Task<Bool>
-    {
+    func buyProductAsyncWithIdentifier(_ productIdentifier: String) -> Task<Bool> {
         let source = TaskCompletionSource<Bool>()
         
-        if !canPurchase() {
-            let error = NSError(domain: "EsyFit can not use use In-App Purchase. Please authorize payment.", code: 0, userInfo: nil)
+        guard canPurchase() else {
+            let error = NSError(domain: "Can not use use In-App Purchase. Please authorize payment.", code: 0, userInfo: nil)
             source.set(error: error)
+            return source.task
         }
-        else {
-            for product in validProducts! {
-                if product.productIdentifier == productIdentifier {
-                    transactionObserver.handle(productIdentifier, runOnceBlock: { (error) in
-                        if let error = error {
-                            source.set(error: error)
-                        } else {
-                            source.set(result: true)
-                        }
-                    })
-                    let payment = SKPayment(product: product)
-                    paymentQueue.add(payment)
+        
+        for product in validProducts! {
+            if product.productIdentifier != productIdentifier { continue }
+            
+            transactionObserver.handle(productIdentifier, runOnceBlock: { (error) in
+                if let error = error {
+                    source.set(error: error)
+                } else {
+                    source.set(result: true)
                 }
-            }
+            })
+            let payment = SKPayment(product: product)
+            paymentQueue.add(payment)
         }
+        
         return source.task
     }
     
-    func canPurchase() -> Bool
-    {
+    func canPurchase() -> Bool {
         return SKPaymentQueue.canMakePayments()
     }
-    
-    ///--------------------------------------
-    // MARK - Accessors
-    ///--------------------------------------
-    
-    lazy var paymentQueue: SKPaymentQueue = {
-        return SKPaymentQueue.default()
-    }()
-    
-    lazy var transactionObserver: PaymentTransactionObserver = {
-        let _transactionObserver = PaymentTransactionObserver()
-        self.paymentQueue.add(_transactionObserver)
-        return _transactionObserver
-    }()
 }
 
